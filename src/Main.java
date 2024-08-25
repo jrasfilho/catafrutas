46c97f2562965e14a4b697f011c3d7deee2f15ec
JOSE RENATO ARAUJO SANTANA FILHO <113191732+jrasfilho@users.noreply.github.com>
2024-08-25 15:51:02 -0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
 package jogo;
 
 import java.util.Random;
+import java.util.List;
+import java.util.ArrayList;
 
 public class Floresta {
     private Elemento[][] terreno;
@@ -21,10 +23,177 @@ public class Floresta {
                 terreno[i][j] = new Grama(i, j);
             }
         }
-        // Adicionar pedras e árvores
+        // Adicionar pedras e árvores (implementação simplificada)
+        int numPedras = dimensao * dimensao / 10; // 10% do terreno são pedras
+        for (int i = 0; i < numPedras; i++) {
+            int x = rand.nextInt(dimensao);
+            int y = rand.nextInt(dimensao);
+            terreno[x][y] = new Pedra(x, y);
+        }
+        // Adicionar árvores:
+        int numArvores = dimensao * dimensao / 10; // ??% do terreno são arvores
+        for (int i = 0; i < numArvores; i++) {
+            int x = rand.nextInt(dimensao);
+            int y = rand.nextInt(dimensao);
+            terreno[x][y] = new Arvore(x, y);
+        }
+        // Adicionar frutas:
+        int numFrutas = dimensao * dimensao / 10; // ??% do terreno são frutas
+        for (int i = 0; i < numFrutas; i++) {
+            int x = rand.nextInt(dimensao);
+            int y = rand.nextInt(dimensao);
+            terreno[x][y] = new Fruta(x, y);
+        }
     }
 
     public void exibirEstado() {
-        // Lógica para exibir o estado atual da floresta
+        for (int i = 0; i < dimensao; i++) {
+            for (int j = 0; j < dimensao; j++) {
+                System.out.print(terreno[i][j].getSimbolo() + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public Elemento getElemento(int x, int y) {
+        return terreno[x][y];
+    }
+
+    public void setElemento(int x, int y, Elemento elemento) {
+        terreno[x][y] = elemento;
+    }
+}
+
+public interface Elemento {
+    void interagir(Jogador jogador);
+    char getSimbolo();
+}
+
+public class Grama implements Elemento {
+    private int x, y;
+
+    public Grama(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    @Override
+    public void interagir(Jogador jogador) {
+        // Lógica para interação com a grama ???????
+    }
+
+    @Override
+    public char getSimbolo() {
+        return '.';
+    }
+}
+
+public class Pedra implements Elemento {
+    private int x, y;
+
+    public Pedra(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    @Override
+    public void interagir(Jogador jogador) {
+        // Lógica para interação com a pedra ?????????????????????
+    }
+
+    @Override
+    public char getSimbolo() {
+        return '#';
+    }
+}
+
+public class Jogador {
+    private String nome;
+    private int x, y;
+    private List<Fruta> mochila;
+    private int pontosMovimento;
+
+    public Jogador(String nome, int x, int y) {
+        this.nome = nome;
+        this.x = x;
+        this.y = y;
+        this.mochila = new ArrayList<>();
+        this.pontosMovimento = 0;
+    }
+
+    public void mover(int direcao, Floresta floresta) {
+        // Implementar lógica de movimento
+        // Não sei como faz isso ainda
+        int novoX = x, novoY = y;
+        switch (direcao) {
+            case 0: novoY--; break; // Cima
+            case 1: novoX++; break; // Direita
+            case 2: novoY++; break; // Baixo
+            case 3: novoX--; break; // Esquerda
+        }
+        if (novoX >= 0 && novoX < floresta.getDimensao() && novoY >= 0 && novoY < floresta.getDimensao()) {
+            Elemento elemento = floresta.getElemento(novoX, novoY);
+            elemento.interagir(this);
+            if (!(elemento instanceof Pedra)) { // Assumindo que não pode passar por pedras
+                x = novoX;
+                y = novoY;
+            }
+        }
+    }
+
+    public void coletarFruta(Fruta fruta) {
+        mochila.add(fruta);
+    }
+
+    // Getters e setters...
+}
+
+public class Jogo {
+    private Floresta floresta;
+    private Jogador[] jogadores;
+    private int rodadaAtual;
+
+    public Jogo(int dimensaoFloresta, String[] nomesJogadores) {
+        this.floresta = new Floresta(dimensaoFloresta);
+        this.jogadores = new Jogador[2];
+        this.rodadaAtual = 0;
+
+        Random rand = new Random();
+        for (int i = 0; i < 2; i++) {
+            int x = rand.nextInt(dimensaoFloresta);
+            int y = rand.nextInt(dimensaoFloresta);
+            jogadores[i] = new Jogador(nomesJogadores[i], x, y);
+        }
+    }
+
+    public void iniciarJogo() {
+        while (!jogoTerminado()) {
+            executarRodada();
+        }
+        anunciarVencedor();
+    }
+
+    private void executarRodada() {
+        for (Jogador jogador : jogadores) { 
+            // Implementar lógica de turno do jogador ??????????????
+        }
+        rodadaAtual++;
+    }
+
+    private boolean jogoTerminado() {
+        // Implementar condição de término do jogo ?????????????? possivelmente com cases!
+        return false;
+    }
+
+    private void anunciarVencedor() {
+        // Implementar lógica para determinar e anunciar o vencedor ??????????????
+    }
+}
+
+public class Principal {
+    public static void main(String[] args) {
+        String[] nomesJogadores = {"Jogador 1", "Jogador 2"};
+        Jogo jogo = new Jogo(10, nomesJogadores);
+        jogo.iniciarJogo();
     }
 }
